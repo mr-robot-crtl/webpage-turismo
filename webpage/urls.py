cb09cd53dd531bbe15e55866e1818f27102072ea
Noe Perez <71856106+mr-robot-crtl@users.noreply.github.com>
2022-05-04 23:02:28 -0500

    reconfigurando eleccion-servicios y  opiniones-clientes

--- webpage/urls.py
+++ webpage/urls.py
@@ -1,7 +1,9 @@
+from unicodedata import name
 from django.urls import path
 from webpage import views
 
 
 urlpatterns = [
     path('',views.home, name="Home"),
+    path('about-pucallpa',views.about_pucallpa, name="about-pucallpa")
 ]
