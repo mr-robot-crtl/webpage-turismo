a59b5d9f08c94aa55b2faba95754acf6bbb1d320
joseazaz31 <73499074+joseazaz31@users.noreply.github.com>
2022-05-08 23:21:23 -0500

    Merge branch 'PROGRAMMING1' of https://github.com/joseazaz31/webpage-turismo into PROGRAMMING1

--- Places/urls.py
+++ Places/urls.py
@@ -5,5 +5,6 @@ from Places import views
 urlpatterns = [
     path('',views.places, name="Places"),
     path('place_cat_aventu',views.place_cat_aventu, name="Place_cat_aventu"),
+    path('lug_cat_aventu_lug_01',views.lug_cat_aventu_lug_01, name="lug_cat_aventu_lug_01"),
 ]
 
--- webpage/views.py
+++ webpage/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from requests import request
 
 # Create your views here.
 
 def home(request):
     return render(request, "webpage/home.html")
+
+def about_pucallpa(request):
+    return render(request, "webpage/about-pucallpa.html")
