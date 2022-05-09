5d32ba2eb5e169b61e8e10f6ef8e7885ae9fc8d4
Noe Perez <71856106+mr-robot-crtl@users.noreply.github.com>
2022-05-08 21:03:12 -0500

    creando pagina about-pucallpa

--- Places/views.py
+++ Places/views.py
@@ -6,3 +6,5 @@ def places(request):
      return render(request, "Places/places.html")
 def place_cat_aventu(request):
     return render(request, "Places/place_cat_aventu.html")
+def lug_cat_aventu_lug_01(request):
+    return render(request, "Places/lug_cat_aventu_lug_01.html")
