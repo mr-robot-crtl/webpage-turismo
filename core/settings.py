874b797d881cef4a42cb3cdcbb72f7eb2380f64d
joseazaz31 <73499074+joseazaz31@users.noreply.github.com>
2022-05-17 16:48:58 -0500

    conexion postgresql

--- core/settings.py
+++ core/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'response',
+        'USER': 'postgres',
+        'PASSWORD' : 'joseazaz',
+        'HOST' : 'localhost',
+        'PORT' : 8080
         #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
