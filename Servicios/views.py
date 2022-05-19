0770284d028fb0c42602584397aab36c7d87e89f
Noe Perez <71856106+mr-robot-crtl@users.noreply.github.com>
2022-05-19 09:53:30 -0500

    realizando el frontend servicios

--- Servicios/views.py
+++ Servicios/views.py
@@ -1,27 +1,40 @@
+from ast import If
+from ctypes import FormatError
 import imp
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from Servicios.models import PlanServicio, CategoriaServicio
+from .forms import FormularioContacto
+from django.core.mail import EmailMessage
 # Create your views here.
 
 def servicio(request):
-     categoria=CategoriaServicio.objects.all()
-     return render(request, "Servicios/services.html", {"categoria": categoria})
+     plan=PlanServicio.objects.all()
+     formulario_contacto=FormularioContacto()
+     if request.method=="POST":
+          formulario_contacto=FormularioContacto(data=request.POST)
+          if formulario_contacto.is_valid():
+               nombre=request.POST.get("nombre")
+               email=request.POST.get("email")
+               contenido=request.POST.get("contenido")
+
+               email=EmailMessage("Mensaje desde app Django", 
+               "EL usuarrio con nombre {} con la direccion {} escribe lo siguiente:\n\n {}".format(nombre,email,contenido),
+               "",["juancitodelbarrio47@gmail.com"],reply_to=[email])
+
+               try:
+                    email.send()
+                    return redirect("/servicio/?valido")
+               except:
+                    return redirect("/servicio/?novalido")
+
+     return render(request, "Servicios/services.html", {"plan": plan, 'miFormulario':formulario_contacto})
 
 def planServicio(request,):
      plan=PlanServicio.objects.all()
      return render(request, "Servicios/planServicio.html", {"plan": plan})
 
-
-
-     #plan=PlanServicio.objects.all()
-     #return render(request, "Servicess/planServicio.html", {"plan": plan})
-
-
 def categoriaServicio(request, planservicio_id):
      plan=PlanServicio.objects.get(id=planservicio_id)
      categoria=CategoriaServicio.objects.filter(planes=plan)
      return render(request, "Servicios/categoriaServicio.html", {"categoria": categoria,"plan":plan})
-
-     #categoria=CategoriaServicio.objects.all()
-     #return render(request, "Servicios/categoriaServicio.html", {"categoria": categoria})
-
+    
--- core/settings.py
+++ core/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'AstralTour_db',
+        'USER': 'postgres',
+        'PASSWORD':'joseazaz',
+        'HOST': 'localhost',
+        'PORT': 8080
         #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
@@ -138,6 +142,13 @@ MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+#config email
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER="juancitodelbarrio47@gmail.com"
+EMAIL_HOST_PASSWORD="juandelbarriopkmz"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK='bootstrap'
