353fdd8075917cb51ea400f81a8fda69a0d171b3
joseazaz31 <73499074+joseazaz31@users.noreply.github.com>
2022-05-20 13:04:42 -0500

    implementando informacion de lugares

--- Places/urls.py
+++ Places/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('',views.places, name="Places"),
     path('place_cat_aventu',views.place_cat_aventu, name="Place_cat_aventu"),
     path('lug_cat_aventu_lug_01',views.lug_cat_aventu_lug_01, name="lug_cat_aventu_lug_01"),
+    path('lug_cat_aventu_lug_02',views.lug_cat_aventu_lug_02, name="lug_cat_aventu_lug_02"),
+    path('lug_cat_turismo',views.lug_cat_turismo, name="lug_cat_turismo"),
+    path('lug_cat_turismo_lug_01',views.lug_cat_turismo_lug_01, name="lug_cat_turismo_lug_01"),
+    path('lug_cat_turismo_lug_02',views.lug_cat_turismo_lug_02, name="lug_cat_turismo_lug_02"),
 ]
 
--- Places/views.py
+++ Places/views.py
@@ -5,6 +5,17 @@ from django.shortcuts import render, HttpResponse
 def places(request):
      return render(request, "Places/places.html")
 def place_cat_aventu(request):
-    return render(request, "Places/place_cat_aventu.html")
+    return render(request, "cat_aventura/place_cat_aventu.html")
 def lug_cat_aventu_lug_01(request):
-    return render(request, "Places/lug_cat_aventu_lug_01.html")
+    return render(request, "cat_aventura/lug_cat_aventu_lug_01.html")
+def lug_cat_aventu_lug_02(request):
+    return render(request, "cat_aventura/lug_cat_aventu_lug_02.html")
+
+def lug_cat_turismo(request):
+    return render(request, "cat_Turismo/lug_cat_turismo.html")
+
+def lug_cat_turismo_lug_01(request):
+    return render(request, "cat_Turismo/lug_cat_turismo_lug_01.html")
+
+def lug_cat_turismo_lug_02(request):
+    return render(request, "cat_Turismo/lug_cat_turismo_lug_02.html")
