fec6eee61980c69a9decd348fa0b344eb2ebc2b5
joseazaz31 <73499074+joseazaz31@users.noreply.github.com>
2022-05-23 12:03:40 -0500

    finalando la implementacion los direccionamiento de las categorias lugares

--- Places/urls.py
+++ Places/urls.py
@@ -7,8 +7,34 @@ urlpatterns = [
     path('place_cat_aventu',views.place_cat_aventu, name="Place_cat_aventu"),
     path('lug_cat_aventu_lug_01',views.lug_cat_aventu_lug_01, name="lug_cat_aventu_lug_01"),
     path('lug_cat_aventu_lug_02',views.lug_cat_aventu_lug_02, name="lug_cat_aventu_lug_02"),
+    path('lug_cat_aventu_lug_03',views.lug_cat_aventu_lug_03, name="lug_cat_aventu_lug_03"),
+    
     path('lug_cat_turismo',views.lug_cat_turismo, name="lug_cat_turismo"),
     path('lug_cat_turismo_lug_01',views.lug_cat_turismo_lug_01, name="lug_cat_turismo_lug_01"),
     path('lug_cat_turismo_lug_02',views.lug_cat_turismo_lug_02, name="lug_cat_turismo_lug_02"),
+    
+    path('lug_cat_comid',views.lug_cat_comid, name="lug_cat_comid"),
+    path('lug_cat_comid_lug_01',views.lug_cat_comid_lug_01, name="lug_cat_comid_lug_01"),
+    path('lug_cat_comid_lug_02',views.lug_cat_comid_lug_02, name="lug_cat_comid_lug_02"),
+    
+    path('lug_cat_seguri',views.lug_cat_seguri, name="lug_cat_seguri"),
+    path('lug_cat_seguri_lug_01',views.lug_cat_seguri_lug_01, name="lug_cat_seguri_lug_01"),
+    path('lug_cat_seguri_lug_02',views.lug_cat_seguri_lug_02, name="lug_cat_seguri_lug_02"),
+    
+    path('lug_cat_educac',views.lug_cat_educac, name="lug_cat_educac"),
+    path('lug_cat_educac_lug_01',views.lug_cat_educac_lug_01, name="lug_cat_educac_lug_01"),
+    path('lug_cat_educac_lug_02',views.lug_cat_educac_lug_02, name="lug_cat_educac_lug_02"),
+    
+    path('lug_cat_salud',views.lug_cat_salud, name="lug_cat_salud"),
+    path('lug_cat_salud_lug_01',views.lug_cat_salud_lug_01, name="lug_cat_salud_lug_01"),
+    path('lug_cat_salud_lug_02',views.lug_cat_salud_lug_02, name="lug_cat_salud_lug_02"),
+    
+    path('lug_cat_banco',views.lug_cat_banco, name="lug_cat_banco"),
+    path('lug_cat_banco_lug_01',views.lug_cat_banco_lug_01, name="lug_cat_banco_lug_01"),
+    path('lug_cat_banco_lug_02',views.lug_cat_banco_lug_02, name="lug_cat_banco_lug_02"),
+    
+    path('lug_cat_zoRecre',views.lug_cat_zoRecre, name="lug_cat_zoRecre"),
+    path('lug_cat_zoRecre_lug_01',views.lug_cat_zoRecre_lug_01, name="lug_cat_zoRecre_lug_01"),
+    path('lug_cat_zoRecre_lug_02',views.lug_cat_zoRecre_lug_02, name="lug_cat_zoRecre_lug_02"),
 ]
 
--- Places/views.py
+++ Places/views.py
@@ -10,12 +10,59 @@ def lug_cat_aventu_lug_01(request):
     return render(request, "cat_aventura/lug_cat_aventu_lug_01.html")
 def lug_cat_aventu_lug_02(request):
     return render(request, "cat_aventura/lug_cat_aventu_lug_02.html")
-
+def lug_cat_aventu_lug_03(request):
+    return render(request, "cat_aventura/lug_cat_aventu_lug_03.html")
+#categorias de turismo
 def lug_cat_turismo(request):
     return render(request, "cat_Turismo/lug_cat_turismo.html")
-
 def lug_cat_turismo_lug_01(request):
     return render(request, "cat_Turismo/lug_cat_turismo_lug_01.html")
-
 def lug_cat_turismo_lug_02(request):
     return render(request, "cat_Turismo/lug_cat_turismo_lug_02.html")
+#categorias comida
+def lug_cat_comid(request):
+    return render(request, "cat_comida/lug_cat_comid.html")
+def lug_cat_comid_lug_01(request):
+    return render(request, "cat_comida/lug_cat_comid_lug_01.html")
+def lug_cat_comid_lug_02(request):
+    return render(request, "cat_comida/lug_cat_comid_lug_02.html")
+#categorias seguridad
+def lug_cat_seguri(request):
+    return render(request, "cat_seguridad/lug_cat_seguri.html")
+def lug_cat_seguri_lug_01(request):
+    return render(request, "cat_seguridad/lug_cat_seguri_lug_01.html")
+def lug_cat_seguri_lug_02(request):
+    return render(request, "cat_seguridad/lug_cat_seguri_lug_02.html")
+
+#categorias educacion
+def lug_cat_educac(request):
+    return render(request, "cat_educacion/lug_cat_educac.html")
+def lug_cat_educac_lug_01(request):
+    return render(request, "cat_educacion/lug_cat_educac_lug_01.html")
+def lug_cat_educac_lug_02(request):
+    return render(request, "cat_educacion/lug_cat_educac_lug_02.html")
+
+#categorias salud
+def lug_cat_salud(request):
+    return render(request, "cat_salud/lug_cat_salud.html")
+def lug_cat_salud_lug_01(request):
+    return render(request, "cat_salud/lug_cat_salud_lug_01.html")
+def lug_cat_salud_lug_02(request):
+    return render(request, "cat_salud/lug_cat_salud_lug_02.html")
+
+#categorias banco
+def lug_cat_banco(request):
+    return render(request, "cat_banco/lug_cat_banco.html")
+def lug_cat_banco_lug_01(request):
+    return render(request, "cat_banco/lug_cat_banco_lug_01.html")
+def lug_cat_banco_lug_02(request):
+    return render(request, "cat_banco/lug_cat_banco_lug_02.html")
+
+
+#categorias zona recreativas
+def lug_cat_zoRecre(request):
+    return render(request, "cat_zonaRecreativa/lug_cat_zoRecre.html")
+def lug_cat_zoRecre_lug_01(request):
+    return render(request, "cat_zonaRecreativa/lug_cat_zoRecre_lug_01.html")
+def lug_cat_zoRecre_lug_02(request):
+    return render(request, "cat_zonaRecreativa/lug_cat_zoRecre_lug_02.html")
