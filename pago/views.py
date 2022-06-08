ef3665308397910e4daa0d98a5ec836f7bb69d20
Noe Perez <71856106+mr-robot-crtl@users.noreply.github.com>
2022-06-07 23:13:14 -0500

    reconfigurando form reserva 50%

--- pago/views.py
+++ pago/views.py
@@ -34,20 +34,29 @@ def cliente_address_view(request):
             email = addressForm.cleaned_data['Email']
             mobile=addressForm.cleaned_data['Mobile']
             address = addressForm.cleaned_data['Address']
+            fecha = addressForm.cleaned_data['fecha']
+            num_a = addressForm.cleaned_data['num_a']
+            num_n = addressForm.cleaned_data['num_n']
+            
             #for showing total price on payment page.....accessing id from cookies then fetching  price of servicio from db
             total=0
+
             if 'servicio_ids' in request.COOKIES:
                 servicio_ids = request.COOKIES['servicio_ids']
                 if servicio_ids != "":
                     servicio_id_in_cart=servicio_ids.split('|')
                     servicios=Servicio.objects.all().filter(id__in = servicio_id_in_cart)
                     for p in servicios:
-                        total=total+p.price
+                        #total=(total+p.price)
+                        total=(num_a*p.price)+(num_n*p.price)
 
             response = render(request, 'ecom/payment.html',{'total':total})
             response.set_cookie('email',email)
             response.set_cookie('mobile',mobile)
             response.set_cookie('address',address)
+            response.set_cookie('fecha',fecha)
+            response.set_cookie('num_a',num_a)
+            response.set_cookie('num_b',num_n)
             return response
     return render(request,'ecom/cliente_address.html',{'addressForm':addressForm,'servicio_in_cart':servicio_in_cart,'servicio_count_in_cart':servicio_count_in_cart})
 
@@ -74,6 +83,9 @@ def payment_success_view(request):
     email=None
     mobile=None
     address=None
+    fecha=None
+    num_a=None
+    num_n=None
     if 'servicio_ids' in request.COOKIES:
         servicio_ids = request.COOKIES['servicio_ids']
         if servicio_ids != "":
@@ -88,12 +100,18 @@ def payment_success_view(request):
         mobile=request.COOKIES['mobile']
     if 'address' in request.COOKIES:
         address=request.COOKIES['address']
+    if 'fecha' in request.COOKIES:
+        fecha=request.COOKIES['fecha']
+    if 'num_a' in request.COOKIES:
+        num_a=request.COOKIES['num_a']
+    if 'num_n' in request.COOKIES:
+        num_n=request.COOKIES['num_n']
 
     # here we are placing number of orders as much there is a servicios
     # suppose if we have 5 items in cart and we place order....so 5 rows will be created in orders table
     # there will be lot of redundant data in orders table...but its become more complicated if we normalize it
     for servicio in servicios:
-        Orders.objects.get_or_create(cliente=cliente,servicio=servicio,status='Pending',email=email,mobile=mobile,address=address)
+        Orders.objects.get_or_create(cliente=cliente,servicio=servicio,status='Pending',email=email,mobile=mobile,address=address,fecha=fecha, num_a=num_a,num_n=num_n)
 
     # after order placed cookies should be deleted
     response = render(request,'ecom/payment_success.html')
@@ -101,4 +119,7 @@ def payment_success_view(request):
     response.delete_cookie('email')
     response.delete_cookie('mobile')
     response.delete_cookie('address')
+    response.delete_cookie('fecha')
+    response.delete_cookie('num_a')
+    response.delete_cookie('num_n')
     return response
